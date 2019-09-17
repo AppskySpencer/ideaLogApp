73d08698f2fc4e95f13fb28afc1f2ab198eaaa74
Spencer Robinson <spencer.robinson259@yahoo.com>
2019-09-17 14:11:32 -0500

    successfully converted idea list to an app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IdeasService } from '../services/ideas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  ideaList: string[];
+  newIdea: string;
 
-  constructor() {}
+  constructor(private ideaService: IdeasService) {}
+
+  ngOnInit() {
+    // get ideas
+    this.ideaList = this.ideaService.getIdeas();
+  }
+
+  // save new idea
+  saveIdea() {
+    if (!this.newIdea || this.newIdea.length === 0) {
+      alert('You need to write what your idea is!');
+    } else {
+      this.ideaList = this.ideaService.saveNewIdea(this.newIdea);
+      this.newIdea = '';
+    }
+  }
+
+  clearList() {
+    this.ideaList = this.ideaService.clearList();
+  }
+
 
 }
